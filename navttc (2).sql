c2a51a75698d86d153fdfa042f62f106ebad52c2
Shabhi Naqvi <shabhinaqvi759@gmail.com>
2024-05-05 22:50:26 +0500

    online store

---
+++ navttc (2).sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 05, 2024 at 07:48 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `navttc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `discription` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`, `discription`, `image`) VALUES
+(1, 'bike', 'aaaaaaaaaaaaaaaaa', 'product-01.jpg'),
+(2, 'bag', 'asas', 'product-02.jpg'),
+(3, 'car', 'aaaaaaaaaaaaaaaaa', 'product-03.jpg'),
+(4, 'watch', 'dfgdgdgf', 'product-04.jpg'),
+(5, 'shert', 'gfgdfg', 'product-05.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persons`
+--
+
+CREATE TABLE `persons` (
+  `id` int(10) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `DOB` date NOT NULL,
+  `age` int(10) NOT NULL DEFAULT 0,
+  `department` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `persons`
+--
+
+INSERT INTO `persons` (`id`, `first_name`, `last_name`, `gender`, `DOB`, `age`, `department`, `salary`) VALUES
+(1, 'Syed', 'Shabhi', 'male', '1996-04-08', 28, 'faculty', 50000),
+(2, 'Nimra', 'mushtaq', 'female', '2003-07-08', 21, 'faculty', 40000),
+(3, 'Asad', 'khan', 'male', '1996-07-08', 28, 'management', 85000),
+(4, 'Adil', 'khan', 'male', '1996-06-05', 29, 'faculty', 30000),
+(5, 'Yaqoob', 'baba', 'male', '1994-04-08', 30, 'management', 43000),
+(6, 'Muzdalfa', 'Khanani', 'female', '2004-07-07', 20, 'management', 25000),
+(7, 'laiba', 'ashraf', 'female', '2000-08-04', 24, 'faculty', 45000),
+(8, 'saba', 'yasmeen', 'female', '1996-04-08', 28, 'faculty', 150000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product` varchar(100) NOT NULL,
+  `discription` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product`, `discription`, `image`, `price`, `quantity`, `category_id`) VALUES
+(1, 'Bag', '1', 'askfljlsdk', 4888, 88, 1),
+(2, 'corolla', 'fgfgfgf', '123.jpg', 4000000, 1, 2),
+(3, 'corolla', 'fgfgfgf', 'about-01.jpg', 4000000, 1, 2),
+(4, 'vitzzz', 'fgfgfgf', 'about-02.jpg', 4000000, 1, 3),
+(5, 'corolla', 'fgfgfgf', 'banner-07.jpg', 4000000, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(10) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `city` varchar(200) DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `salary` varchar(100) DEFAULT NULL,
+  `department` varchar(200) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `email`, `age`, `city`, `gender`, `salary`, `department`, `address`) VALUES
+(1, 'Shabhi', 'Shabhi@gma', 27, 'karachi', 'male', '42500', 'management', 'malir'),
+(2, 'nimra', 'nimra@gmai', 21, 'karachi', 'female', '49178', 'technical', 'gulshan e Iqbal'),
+(3, 'asad', 'asad@gmail', 27, 'karachi', 'male', '37390', 'management', 'malir'),
+(4, 'muneeb', 'muneeb@gma', 32, 'lahore', 'female', '103500', 'technical', 'gulshan e Iqbal'),
+(5, 'mubeen', 'mubeen@gma', 30, 'lahore', 'male', '22500', 'management', 'gulshan e hadeed'),
+(6, 'rafy', 'rafy@gmail', 28, 'lahore', 'female', '53500', 'technical', 'North'),
+(7, 'yaqoob', 'yaqoob@gma', 27, 'Islamabad', 'male', '42500', 'management', 'gulshan e hadeed'),
+(8, 'iqra', 'iqra@gmail', 21, 'Islamabad', 'female', '53500', 'technical', 'North');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `UNION_email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
